5e12cae42c5800ecd021cba7071aa21c19fa0e09
rsoroka <rsoroa@luxoft.com>
2020-02-15 21:56:24 +0300

    Updates

---
+++ precache-manifest.ce1d42f15515c9df3ca7b5908a388bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c7b61fa1b74b26114b0aa58ef4386a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d4a8c795cafb5e22622",
+    "url": "/static/css/main.000b7835.chunk.css"
+  },
+  {
+    "revision": "0970426f993f38baaae9",
+    "url": "/static/js/2.d2f6607a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d2f6607a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4a8c795cafb5e22622",
+    "url": "/static/js/main.b01323a3.chunk.js"
+  },
+  {
+    "revision": "87aa88f30f6cd41085c4",
+    "url": "/static/js/runtime-main.4c613969.js"
+  }
+]);
